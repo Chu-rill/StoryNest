79928235e289c7ac496fd9d6a2aea59a416b57ab
Chu-rill <churchillrimamskep@gmail.com>
2024-08-15 18:25:54 +0100

    added frontend url to cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://nspire-xrhy.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
